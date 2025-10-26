189fff0087084b3625df59b3dc300871591b3b6f
artin paravar <artinparavar86@gmail.com>
2025-10-26 10:26:01 +0330

    first commit

---
+++ Lottery207.js
@@ -0,0 +1,23 @@
+// dark mode
+const toggleSwitch = document.getElementById("input-darkmode");
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    localStorage.removeItem("theme");
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+}
+toggleSwitch.addEventListener("change", switchTheme, false);
+//function
+if (localStorage.getItem("theme") == "dark") {
+  localStorage.setItem("theme", "dark");
+  toggleSwitch.checked = true;
+  document.documentElement.setAttribute("data-theme", "dark");
+} else {
+  toggleSwitch.checked = false;
+  localStorage.removeItem("theme");
+  document.documentElement.setAttribute("data-theme", "light");
+}
